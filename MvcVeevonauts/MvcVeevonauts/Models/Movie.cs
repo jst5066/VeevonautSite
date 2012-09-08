a220b4396cc3bc631d917f45a8d0233443dcd18c
Jared Thompson <jst5066@gmail.com>
2012-09-08 14:30:15 -0400

    Added formatting to Movies table

--- MvcVeevonauts/MvcVeevonauts/Models/Movie.cs
+++ MvcVeevonauts/MvcVeevonauts/Models/Movie.cs
@@ -4,16 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using MvcVeevonauts.Persistence;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcVeevonauts.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Date is required")]
         public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Genre Must be Specified")]
         public string Genre { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Required(ErrorMessage = "Price is required")]
+        [Range(1, 100, ErrorMessage = "Price must be between $1 and $100")]
         public decimal Price { get; set; }
+
+        [StringLength(5)]
+        public string Rating { get; set; }
     }
 
     public class MovieDBContext : DbContext
