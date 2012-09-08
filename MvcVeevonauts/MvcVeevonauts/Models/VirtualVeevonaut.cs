e65b4b8a2b5680de25a388c602c1d8fbbd5bb097
Jared Thompson <jst5066@gmail.com>
2012-09-08 16:57:42 -0400

    Pushing Validation changes to VirtualVeevonauts model

--- MvcVeevonauts/MvcVeevonauts/Models/VirtualVeevonaut.cs
+++ MvcVeevonauts/MvcVeevonauts/Models/VirtualVeevonaut.cs
@@ -10,21 +10,42 @@ namespace MvcVeevonauts.Models
     {
         public int ID { get; set; }
 
-        //[Required(ErrorMessage = "Name is required")]
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(20)]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Type is required")]
+        [StringLength(20)]
         public string Type { get; set; }
 
+        [Required(ErrorMessage = "Intelligence is required")]
+        [Range(1, 10000, ErrorMessage = 
+            "Must be a value from 1 to 10000")]
         public int Intelligence { get; set; }
 
+        [Required(ErrorMessage = "Athleticism is required")]
+        [Range(1, 10000, ErrorMessage =
+            "Must be a value from 1 to 10000")]
         public int Athleticism { get; set; }
 
+        [Required(ErrorMessage = "Strength is required")]
+        [Range(1, 10000, ErrorMessage =
+            "Must be a value from 1 to 10000")]
         public int Strength { get; set; }
 
+        [Required(ErrorMessage = "Speed is required")]
+        [Range(1, 10000, ErrorMessage =
+            "Must be a value from 1 to 10000")]
         public int Speed { get; set; }
 
+        [Required(ErrorMessage = "Artistry is required")]
+        [Range(1, 10000, ErrorMessage =
+            "Must be a value from 1 to 10000")]
         public int Artistry { get; set; }
 
+        [Required(ErrorMessage = "Reaction Time is required")]
+        [Range(1, 10000, ErrorMessage =
+            "Must be a value from 1 to 10000")]
         public int ReactionTime { get; set; }
     }
 }
