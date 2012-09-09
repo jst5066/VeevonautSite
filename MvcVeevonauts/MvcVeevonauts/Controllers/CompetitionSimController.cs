328833cdf9b1ecc0ca94cb7baca633b6bf7c1732
Jared Thompson <jst5066@gmail.com>
2012-09-08 21:04:58 -0400

    Addied CompetitionSim and VirtualVeevonautBook

---
+++ MvcVeevonauts/MvcVeevonauts/Controllers/CompetitionSimController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcVeevonauts.Controllers
+{
+    public class CompetitionSimController : Controller
+    {
+        //
+        // GET: /CompetitionSim/
+
+        public string Index()
+        {
+            return "Coming Soon!";
+        }
+
+    }
+}
---
+++ MvcVeevonauts/MvcVeevonauts/Controllers/VirtualVeevonautBookController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcVeevonauts.Controllers
+{
+    public class VirtualVeevonautBookController : Controller
+    {
+        //
+        // GET: /VirtualVeevonautList/
+
+        public string Index()
+        {
+            return "Virtual Veevonaut Book: List Coming Soon!";
+        }
+
+        public string Browse()
+        {
+            return "Virtual Veevonaut Book: Browsing Coming Soon!";
+        }
+
+        public string Details()
+        {
+            return "Virtual Veevonaut Book: Details Coming Soon!";
+        }
+
+    }
+}
